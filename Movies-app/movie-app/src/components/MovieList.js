f86b1754d7eecb932b89a41e99ca0d616936454d
edwin-magairwa <edwinmagairwa@gmail.com>
2025-04-26 21:43:42 +0300

    Modifie MovieList.js

--- Movies-app/movie-app/src/components/MovieList.js
+++ Movies-app/movie-app/src/components/MovieList.js
@@ -6,6 +6,15 @@ function MovieList({ movies, loading, error }) {
   if (error) return <div className="error">{error}</div>;
 
   return (
-    
+ <div className="movie-list">
+      <h2>Popular Movies</h2>
+      {movies.map((movie) => (
+        <MovieCard
+          key={movie.id}
+          movie={{
+            
+    </div>
+  );
+}   
 
 export default MovieList;
