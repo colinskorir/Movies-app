bace8626dcd60a6ed73fe88271a144924c6b3aa4
colinskorir <korircoll20@gmail.com>
2025-04-26 23:41:25 +0300

    WatchListForm modified

--- Movies-app/movie-app/src/components/WatchListForm.js
+++ Movies-app/movie-app/src/components/WatchListForm.js
@@ -2,6 +2,18 @@ import React, { useState } from "react";
 
 function WatchlistForm({ addMovie }) {
   const [formData, setFormData] = useState({
-  
+    title: "",
+    year: "",
+    rating: "",
+    poster: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+ 
+}
 
 export default WatchlistForm;
