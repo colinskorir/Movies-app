f10728a5107bce4ea7d758b944d68af8b35ecaca
edwin-magairwa <edwinmagairwa@gmail.com>
2025-04-26 21:40:43 +0300

    modified MovieList.js

--- Movies-app/movie-app/src/components/MovieList.js
+++ Movies-app/movie-app/src/components/MovieList.js
@@ -4,3 +4,8 @@ import MovieCard from "./MovieCard";
 function MovieList({ movies, loading, error }) {
   if (loading) return <div className="loading">Loading...</div>;
   if (error) return <div className="error">{error}</div>;
+
+  return (
+    
+
+export default MovieList;
