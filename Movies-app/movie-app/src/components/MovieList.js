d12f23f0586717db00a227ea2d377887d0ac16c6
edwin-magairwa <edwinmagairwa@gmail.com>
2025-04-26 21:38:34 +0300

    added movies

---
+++ Movies-app/movie-app/src/components/MovieList.js
@@ -0,0 +1,6 @@
+import React from "react";
+import MovieCard from "./MovieCard";
+
+function MovieList({ movies, loading, error }) {
+  if (loading) return <div className="loading">Loading...</div>;
+  if (error) return <div className="error">{error}</div>;
